e05664c88a39efaae46bb572a689c3e936dd90ea
kyumoon bae <bgm8547@gmail.com>
2019-04-10 14:52:42 +0900

    iframe크기 동적으로 설정되도록 수정

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -10,21 +10,23 @@ class VideoDetail extends React.Component {
         this.title = item ? item.snippet.title : "empty";
         const videoId = item ? item.id.videoId : ''
         return (
-            <div className="video-detail">
-                {videoId?
-                    <iframe width="560" height="315" src={"https://www.youtube.com/embed/"+videoId} 
-                    frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-                    allowfullscreen>
-                    </iframe>
-                    :<div>...loading</div>
-                }
+            <div>
+                <div className="video-detail">
+                    {videoId?
+                        <iframe width="100%" height="100%" src={"https://www.youtube.com/embed/"+videoId} 
+                        frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                        allowfullscreen="true">
+                        </iframe>
+                        :<div>...loading</div>
+                    }
+                </div>
                 <div className="title" ref={this.ref} style={{'WebkitBoxOrient': 'vertical'}}></div>
             </div>
         )
     }
 
     componentDidUpdate(){
-        console.log('title:',this.title);
+        // width="560" height="315"
         if(this.ref.current){
             this.ref.current.innerHTML=this.title;
         }
